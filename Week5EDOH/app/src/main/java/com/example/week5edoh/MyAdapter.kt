cb6229ae505f79785a370145fbb258e3f703abc5
greatdad <85246406+greatdad@users.noreply.github.com>
2021-07-06 08:23:59 +0100

    Update MyAdapter.kt

--- Week5EDOH/app/src/main/java/com/example/week5edoh/MyAdapter.kt
+++ Week5EDOH/app/src/main/java/com/example/week5edoh/MyAdapter.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
+import kotlinx.android.synthetic.main.row.view.*
 
 
 class MyAdapter (val arrayList: ArrayList<Model>, val context: Context):
