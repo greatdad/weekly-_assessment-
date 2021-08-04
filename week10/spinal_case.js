60860e7cbb398cfe274030ecfbbdd98104e57eda
greatdad <85246406+greatdad@users.noreply.github.com>
2021-08-04 15:06:02 +0100

    Update spinal_case.js

--- week10/spinal_case.js
+++ week10/spinal_case.js
@@ -1,6 +1,5 @@
 function spinalCase(str) {   
    //algorithm of spinal case
-   str = "Demonstration of a string in spinal case";
    return str.split(/\s|_|(?=[A-Z])/).join("-").toLowerCase(); 
-   console.log(str);
 }
+   spinalCase("Demonstration of a string in spinal case");
